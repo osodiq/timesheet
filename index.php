f3360fb8b5a111e79e472844b42072ae2c76911e
sodiqobalowu <sodiqobalowu@bincom.net>
2017-08-14 08:48:35 +0100

    [firstcomit]timesheet

---
+++ index.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+<html>
+	<head>
+		<title>TimeSheet</title>
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="assets/css/material-kit.css" rel="stylesheet"/>
+        <link rel="stylesheet" type="text/css" href="main.css">
+		<meta name="viewport" content="width=device-width initial-scale=1">
+		<!--JS  -->
+        <script src="bootstrap/js/jquery-3.2.1.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+	
+	</head>
+	<body>
+    <div class="container">
+    </div>
+
+    </body>
+</hmtl>
